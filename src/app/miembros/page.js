31df86bc8e5352f3a5195b39a57aecb0614d7bb0
Bill Andy <billandyqm@gmail.com>
2024-12-26 18:19:54 -0500

    last_configs

--- src/app/miembros/page.js
+++ src/app/miembros/page.js
@@ -11,7 +11,6 @@ export default function Home() {
             const data = await response.json();
             setMiembros(Array.isArray(data) ? data : []);
         };
-
         fetchMiembros();
     }, []);
 
